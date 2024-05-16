f4e716dda7babc4553f1c1785ba7a872d1d42f0b
Мария Доронина <147492942+vot-blin@users.noreply.github.com>
2024-05-16 21:55:31 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+history = model.fit(x_train, y_train, epochs=5, verbose=1)
+test_loss, test_acc = model.evaluate(x_test, y_test)
+print(f'Точность на тестовом наборе: {test_acc * 100:.2f}%')
+import sys
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+plt.plot(history.history['accuracy'])
+plt.title('Кривая обучения')
+plt.xlabel('Эпоха')
+plt.ylabel('Точность')
+plt.show()
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
